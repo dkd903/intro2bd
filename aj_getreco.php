62937a2add8b0a9c6fb57928f48e393f1ccf2650
Debjit Saha <debjitsaha87@gmail.com>
2014-05-11 21:02:54 -0400

    Simil DIstance

--- aj_getreco.php
+++ aj_getreco.php
@@ -33,7 +33,44 @@ foreach ($films as $key=>$value) {
 	}
 	$nfilms[$key] = $temp;
 }
-$recommendations = generate_reco($nfilms, $_COOKIE["email"]);
+
+
+$total = array();
+$simIlarity_total = array();
+$iranks = array();
+$distmeasure = 0;
+
+$movies_user_ratings = $nfilms;
+$person = $_COOKIE["email"];
+
+foreach($movies_user_ratings as $eachuSer=>$values) {
+    if($eachuSer != $person) {
+        $distmeasure = simil_dist($movies_user_ratings, $person, $eachuSer);
+        $distmeasure = 1/(1 + $distmeasure);
+    }
+    
+    if($distmeasure > 0) {
+        foreach($movies_user_ratings[$eachuSer] as $key=>$value) {
+            if(!array_key_exists($key, $movies_user_ratings[$person])) {
+                if(!array_key_exists($key, $total)) {
+                    $total[$key] = 0;
+                }
+                $total[$key] += $movies_user_ratings[$eachuSer][$key] * $distmeasure;
+                if(!array_key_exists($key, $simIlarity_total)) {
+                    $simIlarity_total[$key] = 0;
+                }
+                $simIlarity_total[$key] += $distmeasure;
+            }
+        }
+        
+    }
+}
+foreach($total as $key=>$value){
+    $iranks[$key] = $value / $simIlarity_total[$key];
+}
+array_multisort($iranks, SORT_DESC);    
+
+$recommendations = $iranks;
 
 $finalReturn = array();
 
